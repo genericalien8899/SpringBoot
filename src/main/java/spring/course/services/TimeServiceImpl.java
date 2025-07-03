4392addb01ab0bd3b7e89e676b65ddc42990f15f
genericalien8899 <mailforgithubsync@gmail.com>
2025-07-03 23:59:42 +0530

    Completed calling an external API from spring boot

--- src/main/java/spring/course/services/TimeServiceImpl.java
+++ src/main/java/spring/course/services/TimeServiceImpl.java
@@ -2,6 +2,7 @@ package spring.course.services;
 
 import org.springframework.stereotype.Service;
 
+import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
 import spring.course.model.TimeAPIResponse;
 
@@ -11,9 +12,9 @@ public class TimeServiceImpl implements TimeService {
     @Override
     public String getCurrentTime(String timezone) {
         
-        TimeAPIResponse response = Unirest.get("http://worldtimeapi.org/api/timezone/Chennai/"+timezone).asObject(TimeAPIResponse.class).getBody();
+        HttpResponse<TimeAPIResponse> response = Unirest.get("http://worldtimeapi.org/api/timezone/Chennai/"+timezone).asObject(TimeAPIResponse.class);
        
-        return response.getUtc_datetime();
+        return response.getBody().getUtc_datetime();
     }
 
 
